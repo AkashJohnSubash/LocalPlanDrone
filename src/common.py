12884aee8eeb801b880f9070a8b7bcfb512ac8f7
AkashJohn <akash31994@gmail.com>
2024-04-19 19:18:07 +0200

    notation change

--- src/common.py
+++ src/common.py
@@ -5,9 +5,9 @@ import casadi as ca
 
 g0  = 9.80665     # [m.s^2] accerelation of gravity
 mq  = 31e-3      # [kg] total mass (with Lighthouse deck)
-Ixx = 1.395e-5   # [kg.m^2] Inertial moment around x-axis
-Iyy = 1.395e-5   # [kg.m^2] Inertial moment around y-axis
-Izz = 2.173e-5   # [kg.m^2] Inertia moment around z-axis
+Ix = 1.395e-5   # [kg.m^2] Inertial moment around x-axis
+Iy = 1.395e-5   # [kg.m^2] Inertial moment around y-axis
+Iz = 2.173e-5   # [kg.m^2] Inertia moment around z-axis
 Cd  = 7.9379e-06 # [N/krpm^2] Drag coef
 Ct  = 3.25e-4    # [N/krpm^2] Thrust coef
 dq  = 92e-3      # [m] distance between motors' center
--- src/sys_dynamics.py
+++ src/sys_dynamics.py
@@ -3,23 +3,23 @@ from common import *
 
 '-------------------Symbolic variables---------------------------'
 # State variables
-x = ca.SX.sym('x');    y = ca.SX.sym('y');    z = ca.SX.sym('z')                         # (in inertial frame)
+x = ca.SX.sym('x');     y = ca.SX.sym('y');    z = ca.SX.sym('z')                         # (in inertial frame)
 q1 = ca.SX.sym('q1');  q2 = ca.SX.sym('q2');  q3 = ca.SX.sym('q3');  q4 = ca.SX.sym('q4')   # quarternion angle representation
-u = ca.SX.sym('u');    v = ca.SX.sym('v');    w = ca.SX.sym('w')                         # linear velocities (in body frame)
-p = ca.SX.sym('p');    q = ca.SX.sym('q');    r = ca.SX.sym('r')                         # angular velocities w.r.t phi(roll), theta(pitch), psi(yaw)
+vx = ca.SX.sym('vx');  vy = ca.SX.sym('vy');  vz = ca.SX.sym('vz')                         # linear velocities (in body frame)
+wr = ca.SX.sym('wr');  wp = ca.SX.sym('wp');  wy = ca.SX.sym('wy')                         # angular velocities w.r.t phi(roll), theta(pitch), psi(yaw)
 
-state = ca.vertcat(x, y, z, q1, q2, q3, q4, u, v, w, p, q, r)
+zeta = ca.vertcat(x, y, z, q1, q2, q3, q4, vx, vy, vz, wr, wp, wy)
 
 # Control variable angles (Motor RPM)
-w1 = ca.SX.sym('w1');  w2 = ca.SX.sym('w2');  w3 = ca.SX.sym('w3');  w4 = ca.SX.sym('w4')
-controls = ca.vertcat( w1, w2, w3, w4) 
+ua = ca.SX.sym('ua');  ub = ca.SX.sym('ub');  uc = ca.SX.sym('uc');  ud = ca.SX.sym('ud')
+u = ca.vertcat( ua, ub, uc, ud) 
 
 class SysDyn():
     
     def __init__(self):
         
-        self.stSize = state.numel()
-        self.ctrlSize = controls.numel()
+        self.stSize = zeta.numel()
+        self.ctrlSize = u.numel()
 
         # matrices containing all States, Controls, Paramters over all time steps +1
         self.St = ca.SX.sym('St', self.stSize, hznLen + 1)
@@ -30,33 +30,33 @@ class SysDyn():
     def ForwardDynamics(self):
         '''ODEs for system dynamic model'''
 
-        # Rate of change of position
+        #Rate of change of position
         dx = u*(2*(q1**2 + q2**2)- 1)  + v*2*(q2*q3 - q1*q4)           + w*2*(q1*q3 + q2*q4)
         dy = u*2*(q1*q4 + q2*q3)       + v*(2*(q1**2 + q3**2) - 1)     + w*2*(q3*q4 - q1*q2)
         dz = u*2*(q2*q4 - q1*q3)       + v*2*(q1*q2 + q3*q4)           + w*(2*(q1**2 + q4**2) - 1) 
         
-        # dx = u
-        # dy = v
-        # dz = w
+        # dx = vx
+        # dy = vy
+        # dz = vz
 
         # Rate of change of angles (in qaurternion)
-        dq1 = - (q2*p)/2 - (q3*q)/2 - (q4*r)/2
-        dq2 =   (q1*p)/2 - (q4*q)/2 + (q3*r)/2
-        dq3 =   (q4*p)/2 + (q1*q)/2 - (q2*r)/2
-        dq4 = - (q3*p)/2 + (q2*q)/2 + (q1*r)/2        
+        dq1 = - (q2 * wr)/2 - (q3 * wp)/2 - (q4 * wy)/2
+        dq2 =   (q1 * wr)/2 - (q4 * wp)/2 + (q3 * wy)/2
+        dq3 =   (q4 * wr)/2 + (q1 * wp)/2 - (q2 * wy)/2
+        dq4 = - (q3 * wr)/2 + (q2 * wp)/2 + (q1 * wy)/2        
 
         # Rate of change of linear velocity
-        du =   -w*q  + v*r  + g0*(2*q1*q3 - 2*q2*q4) 
-        dv =    w*p  - u*r  - g0*(2*q1*q2 + 2*q3*q4)
-        dw =   -v*p  + u*q  - g0*(2*q1**2 + 2*q4**2 - 1) + (Ct*(w1**2 + w2**2 + w3**2 + w4**2))/mq
+        du =   -vz * wp  + vy * wy  + 2 * g0 * (q1 * q3 + q2 * q4) 
+        dv =    vz * wr  - vx * wy  - 2 * g0 * (q1 * q2 + q3*  q4)
+        dw =   -vy * wr  + vx * wp  - 2 * g0 * (q1**2 + q4**2 - 0.5) + (Ct*( ua**2 + ub**2 + uc**2 + ud**2))/mq
         
         # Rate of change of angular velocity
-        dp = -(Ct*l*(w1**2 + w2**2 - w3**2 - w4**2) - Iyy*q*r + Izz*q*r)/Ixx
-        dq = -(Ct*l*(w1**2 - w2**2 - w3**2 + w4**2) + Ixx*p*r - Izz*p*r)/Iyy
-        dr = -(Cd*  (w1**2 - w2**2 + w3**2 - w4**2) - Ixx*p*q + Iyy*p*q)/Izz
+        dp = -(Ct*l*(ua**2 + ub**2 - uc**2 - ud**2) - Iy * wp * wy + Iz * wp * wy) / Ix
+        dq = -(Ct*l*(ua**2 - ub**2 - uc**2 + ud**2) + Ix * wr * wy - Iz * wr * wy) / Iy
+        dr = -(Cd*  (ua**2 - ub**2 + uc**2 - ud**2) - Ix * wr * wp + Iy * wr * wp) / Iz
         
         f_op = ca.vertcat(dx, dy, dz, dq1, dq2, dq3, dq4, du, dv, dw, dp, dq, dr)
-        fp = ca.Function('f', [state, controls], [f_op])
+        fp = ca.Function('f', [zeta, u], [f_op])
         
         return fp
 
