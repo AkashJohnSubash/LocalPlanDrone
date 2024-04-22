2b0938f3b5480b8b82d098ec831f25135c4079de
AkashJohn <akash31994@gmail.com>
2024-04-22 07:35:35 +0200

    infeasiblity with model change

--- src/sys_dynamics.py
+++ src/sys_dynamics.py
@@ -30,14 +30,14 @@ class SysDyn():
     def ForwardDynamics(self):
         '''ODEs for system dynamic model'''
 
-        #Rate of change of position
-        dx = vx * (2*(q1**2 + q2**2)- 1)  + vy *2*(q2*q3 - q1*q4)           + vz *2*(q1*q3 + q2*q4)
-        dy = vx * 2*(q1*q4 + q2*q3)       + vy *(2*(q1**2 + q3**2) - 1)     + vz *2*(q3*q4 - q1*q2)
-        dz = vx * 2*(q2*q4 - q1*q3)       + vy *2*(q1*q2 + q3*q4)           + vz *(2*(q1**2 + q4**2) - 1) 
+        # #Rate of change of position
+        # dx = vx * (2*(q1**2 + q2**2)- 1)  + vy *2*(q2*q3 - q1*q4)           + vz *2*(q1*q3 + q2*q4)
+        # dy = vx * 2*(q1*q4 + q2*q3)       + vy *(2*(q1**2 + q3**2) - 1)     + vz *2*(q3*q4 - q1*q2)
+        # dz = vx * 2*(q2*q4 - q1*q3)       + vy *2*(q1*q2 + q3*q4)           + vz *(2*(q1**2 + q4**2) - 1) 
         
-        # dx = vx
-        # dy = vy
-        # dz = vz
+        dx = vx
+        dy = vy
+        dz = vz
 
         # Rate of change of angles (in qaurternion)
         dq1 = - (q2 * wr)/2 - (q3 * wp)/2 - (q4 * wy)/2
@@ -45,10 +45,14 @@ class SysDyn():
         dq3 =   (q4 * wr)/2 + (q1 * wp)/2 - (q2 * wy)/2
         dq4 = - (q3 * wr)/2 + (q2 * wp)/2 + (q1 * wy)/2        
 
-        # Rate of change of linear velocity
-        du =   -vz * wp  + vy * wy  + 2 * g0 * (q1 * q3 + q2 * q4) 
-        dv =    vz * wr  - vx * wy  - 2 * g0 * (q1 * q2 + q3*  q4)
-        dw =   -vy * wr  + vx * wp  - 2 * g0 * (q1**2 + q4**2 - 0.5) + (Ct*( ua**2 + ub**2 + uc**2 + ud**2))/mq
+        # # Rate of change of linear velocity
+        # du =   -vz * wp  + vy * wy  + 2 * g0 * (q1 * q3 + q2 * q4) 
+        # dv =    vz * wr  - vx * wy  - 2 * g0 * (q1 * q2 + q3*  q4)
+        # dw =   -vy * wr  + vx * wp  - 2 * g0 * (q1**2 + q4**2 - 0.5) + (Ct*( ua**2 + ub**2 + uc**2 + ud**2))/mq
+
+        du =    2 * Ct/mq *(q1 * q3 + q2 * q4) * (ua**2 + ub**2 + uc**2 + ud**2)
+        dv =   -2 * Ct/mq *(q1 * q2 - q3 * q4) * (ua**2 + ub**2 + uc**2 + ud**2)
+        dw =   g0 + 2 * Ct/mq *(q1**2 + q4**2 -0.5) * (ua**2 + ub**2 + uc**2 + ud**2)
         
         # Rate of change of angular velocity
         dp = -(Ct*l*(ua**2 + ub**2 - uc**2 - ud**2) - Iy * wp * wy + Iz * wp * wy) / Ix
